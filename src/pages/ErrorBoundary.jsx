91c3fb19d3e8d60fde88be8518fbea509efd1d15
helkass <helkaseptyawan@gmail.com>
2023-08-08 21:56:59 +0700

    add 404 page

--- src/pages/ErrorBoundary.jsx
+++ src/pages/ErrorBoundary.jsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+
 const ErrorBoundary = () => {
-   return <div>ErrorBoundary</div>;
+   return (
+      <section className="my-12 min-h-screen">
+         <div className="container text-slate-700 gap-y-3 flex flex-col h-full justify-center items-center">
+            <h1 className="text-7xl xl:text-[10rem] font-bold">404</h1>
+            <h2>Something's missing!</h2>
+            <Link to={"/"} className="underline">
+               Go back!
+            </Link>
+         </div>
+      </section>
+   );
 };
 
 export default ErrorBoundary;
